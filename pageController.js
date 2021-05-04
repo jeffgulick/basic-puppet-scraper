702892ab389a652edb1ab309045b69b1aee05b16
JEFFGULICK <jeffgulick@gmail.com>
2021-05-03 21:10:04 -0500

    finished

--- pageController.js
+++ pageController.js
@@ -1,11 +1,23 @@
 const pageScraper = require('./pageScraper');
+const fs = require('fs');
 
 const scrapeAll = async (browserInstance) => {
   let browser;
   //creates browser instance and calls object method in pagescraper
   try {
     browser = await browserInstance;
-    await pageScraper.scraper(browser);
+    let scrapedData;
+    scrapedData = await pageScraper.scraper(browser);
+    await browser.close();
+    //writes scraped data to json file
+    fs.writeFile('data.json', JSON.stringify(scrapedData), 'utf8', function (
+      err
+    ) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(scrapedData);
+    });
   } catch (err) {
     console.log('Could not resolve the browser instance => ', err);
   }
--- recurrsive.js
+++ recurrsive.js
@@ -1,22 +1,18 @@
 let providerList = [];
 let mergedList = [];
-let scrapedData = [];
+let finalList = [];
 
 const scraperObject = {
-  url: `http://www.npino.org/doctor/dental-providers/dentist-122300000X?page=`,
+  url: `http://www.npino.org/doctor/dental-providers/dentist-122300000X`,
 
   async scraper(browser) {
     let page = await browser.newPage();
     console.log(`Navigating to ${this.url}...`);
+    //navigates to concantnated url
+    await page.goto(this.url);
 
-    //loops through each page of site to be scraped
-    for (let i = 1; i <= 1; i++) {
-      const temp = `http://www.npino.org/doctor/dental-providers/dentist-122300000X?page=`;
-      //changes url on each pass
-      scraperObject.url = temp + i;
-      console.log(scraperObject.url);
-      //navigates to concantnated url
-      await page.goto(this.url);
+    //to run script recurive
+    const recursiveLoop = async () => {
       // Wait for the required DOM to be rendered
       await page.waitForSelector('.npi-record');
       // Get the link to all the required names
@@ -25,16 +21,14 @@ const scraperObject = {
         return links;
       });
 
-      //dumping into array
-      providerList.push(urls);
-      mergedList = providerList.flat();
-
       // Loop through each of those links, open a new page instance and get the relevant data from them
       let pagePromise = (link) =>
+        //create promise to look through records and pull out info
         new Promise(async (resolve, reject) => {
           let dataObj = {};
           let newPage = await browser.newPage();
 
+          //to resolve promise
           try {
             //{**name**}
             await newPage.goto(link);
@@ -58,22 +52,44 @@ const scraperObject = {
             );
             //{**fax}
             dataObj.fax = await newPage.$eval('.fax', (fax) => fax.textContent);
-          } catch (err) {
-            console.log(err);
-          }
 
+            //if promise is rejected, will report error and move on
+          } catch (err) {
+            console.log('Error!!!', err);
+          }
           resolve(dataObj);
           await newPage.close();
         });
-
+      //looping through urls
       for (link in urls) {
         let currentPageData = await pagePromise(urls[link]);
-        scrapedData.push(currentPageData);
+        finalList.push(currentPageData);
         console.log(currentPageData);
       }
-    }
-    console.log('list', mergedList);
-    return scrapedData;
+
+      let nextButtonExist = false;
+
+      //checking for next button;
+      try {
+        await page.$eval('.next > a', (a) => a.textContent);
+        nextButtonExist = true;
+      } catch (err) {
+        nextButtonExist = false;
+      }
+
+      if (nextButtonExist) {
+        await page.click('.next > a');
+        console.log(providerList);
+        return recursiveLoop(); // Call this function recursively
+      } else {
+        mergedList = providerList.flat();
+        console.log('list', mergedList);
+        await page.close();
+        return finalList;
+      }
+    };
+    const data = recursiveLoop();
+    return data;
   },
 };
 
